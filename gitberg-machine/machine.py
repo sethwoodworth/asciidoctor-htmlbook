5fb95874b8ce770abf3a0105e836a0bb26e2cbc3
eric <eric@hellman.net>
2015-10-30 14:49:56 -0400

    added Jinja2 machine

---
+++ gitberg-machine/machine.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+""" templating machine for gitberg
+
+Usage: python machine.py [options] [templatefiles]
+
+Options:
+  -m   put specified metadata file or URL into context
+  -h, --help              show this help
+  -o   put result in -o directory
+
+Example:
+  machine.py -m metadata.yaml content.html   process content.html as a template file with metadata.yaml in the context, save in place
+
+"""
+
+import sys ,getopt, os.path
+from jinja2 import FileSystemLoader, Environment
+from gitenberg.metadata.pandata import Pandata
+
+def main(argv):                          
+    try:                                
+        opts, templatefiles = getopt.getopt(argv, "hm:o:", ["help",])
+    except getopt.GetoptError:          
+        usage()                         
+        sys.exit(2) 
+    metadata_file = None  
+    output_dir = ''                  
+    for opt, arg in opts:                
+        if opt in ("-h", "--help"):      
+            usage()                     
+            sys.exit()                  
+        elif opt == '-m':                
+            metadata_file = arg               
+        elif opt == '-o':                
+            output_dir = arg               
+    if metadata_file:
+        pandata = Pandata(metadata_file)
+    else:
+        pandata = {}
+    for templatefile in templatefiles:
+        template = env.get_template(templatefile)
+        output = template.render(metadata=pandata)
+        outputfile = open(os.path.join(output_dir,templatefile),'w')
+        outputfile.write(output.encode("UTF-8"))
+        outputfile.close()
+
+def usage():
+    print __doc__
+
+tempdir = os.path.join(os.path.dirname(sys.argv[0]), 'templates/')
+print dir
+env = Environment(loader=FileSystemLoader([tempdir,'/',]))
+main(sys.argv[1:])
